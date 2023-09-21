ff76283a813735f15ed997591f84b2fb22e77d3c
FannyMayorga1999 <fannymayorga1999@gmail.com>
2023-09-21 17:30:16 -0500

    Model Categoria

---
+++ demov01/src/main/java/com/example/demov01/dto/CategoriaDto.java
@@ -0,0 +1,24 @@
+package com.example.demov01.dto;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categoria", schema = "private")
+@Data
+public class CategoriaDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String benefit;
+
+    @Column(name = "deleted")
+    private boolean deleted;
+}
+}
