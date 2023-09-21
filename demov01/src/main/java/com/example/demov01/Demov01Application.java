7e6615d6abda6d5b94078413877d2ed7c0142dfe
Your Name <you@example.com>
2023-09-21 16:35:55 -0500

    first commit

---
+++ demov01/src/main/java/com/example/demov01/Demov01Application.java
@@ -0,0 +1,13 @@
+package com.example.demov01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demov01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demov01Application.class, args);
+	}
+
+}
---
+++ demov01/src/test/java/com/example/demov01/Demov01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demov01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demov01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
