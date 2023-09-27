3217487a2c57c77fb19553478095f4eb448db8f8
FannyMayorga1999 <fannymayorga1999@gmail.com>
2023-09-26 22:43:51 -0500

    Angular base de los modulos

--- front-end-angular/src/app/app.module.ts
+++ front-end-angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/pages/dashboard.component';
+import { HomeComponent } from './modules/dashboard/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
