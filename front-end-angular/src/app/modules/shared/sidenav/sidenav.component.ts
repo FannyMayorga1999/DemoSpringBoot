2000f60f3bc197bc8b4cfa3d270ee01eff15f970
FannyMayorga1999 <fannymayorga1999@gmail.com>
2023-10-02 20:46:34 -0500

    Sidenav total

--- front-end-angular/src/app/modules/shared/sidenav/sidenav.component.ts
+++ front-end-angular/src/app/modules/shared/sidenav/sidenav.component.ts
@@ -9,6 +9,23 @@ import { MediaMatcher } from "@angular/cdk/layout";
 export class SidenavComponent implements OnInit {
   mobileQuery: MediaQueryList;
 
+  menuNav = [
+    {
+      name: "Home",
+      route: "home",
+      icon: "home",
+    },
+    {
+      name: "Categoría",
+      route: "category",
+      icon: "category",
+    },
+    {
+      name: "Productos",
+      route: "product",
+      icon: "shopping_cart",
+    },
+  ];
   /**MEDIA Query */
   constructor(media: MediaMatcher) {
     this.mobileQuery = media.matchMedia("max-witdh:600px");
